c6ef954f0f651c5e5244e156cf42a619f8902b20
Dilip <dilip.kumar2k6@gmail.com>
2015-04-25 12:48:06 -0700

    Update TripleDes.java

--- TripleDes.java
+++ TripleDes.java
@@ -1,139 +1,146 @@
-import java.security.GeneralSecurityException;
+/**
+ * Module: jumpstart
+ *
+ * --------------------------------------------------------------------------
+ *
+ * (C) Copyright 2014 American Express, Inc. All rights reserved.
+ * The contents of this file represent American Express trade secrets and
+ * are confidential. Use outside of American Express is prohibited and in
+ * violation of copyright law.  Licence information of all dependent modules
+ * can be found https://stash.mps.intra.aexp.com/projects/WF/repos/jumpstart-engine/browse/README.md
+ *
+ *
+ * Description: jumpstart - <DESC_HERE>
+ */
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.SecretKeySpec;
+'use strict';
+/*
+ *  Offers related services.
+ */
+var crypto = require("crypto");
+module.exports = {
+    encrypt: function (plainText) {
+        //Get encryption key as char string i.e. of 24 size        
+        return encrypt({
+            alg: 'des-ede3', //3des-ecb  
+            autoPad: false,
+            key: "73AD9CEC99816AA6A4D82FB273AD9CEC99816AA6A4D82FB2 ",
+            plaintext: plainText,
+            iv: null
+        });
+    }
+};
+function encrypt(param) {
+    var key = hexToBufferArray(param.key);
+    var iv = new Buffer(param.iv ? param.iv : 0);
+    var plaintext = addPadding(new Buffer(param.plaintext));
+    var alg = param.alg;
+    var autoPad = false;
+    //encrypt  
+    var cipher = crypto.createCipheriv(alg, key, iv);
+    cipher.setAutoPadding(autoPad); //default true  
+    var ciph = cipher.update(plaintext, 'utf8', 'hex');
+    ciph += cipher.final('hex');
+    ciph = ciph.toUpperCase();
+    console.log(alg, ciph);
+    return ciph;
+}
 
 
-public class TripleDes {
-	// Crypto library related keys
-	private static final String ALGO_NAME = "DESede/ECB/NoPadding";
-	private static final int PADDING_BLOCK = 8;
 
-	// Test Data
-	private static final String PLAIN_TEXT = "Hello World";
-	private static final String SHARED_KEY = "73AD9CEC99816AA6A4D82FB273AD9CEC99816AA6A4D82FB2";
+function decrypt(param) {
+    var key = hexToBufferArray(param.key);
+    var iv = new Buffer(param.iv ? param.iv : 0);
+    var decrypttext = removePadding(new Buffer(param.decrypttext));
+    var alg = param.alg;
+    var autoPad = false;
+    //decrypt  
+    var decipher = crypto.createDecipheriv(alg, key, iv);
+    decipher.setAutoPadding(autoPad);
+    var txt = decipher.update(decrypttext, 'hex', 'utf8');
+    txt += decipher.final('utf8');
+    console.log(alg, txt);
+    return txt;
+}
 
-	public static void main(String[] arg) {
+function fromDigit(ch) {
+    var number = 0;
+    if (ch >= '0' && ch <= '9')
+        number = ch.charCodeAt(0) - '0'.charCodeAt(0);
+    if (ch >= 'A' && ch <= 'F')
+        number = ch.charCodeAt(0) - 'A'.charCodeAt(0) + 10;
+    if (ch >= 'a' && ch <= 'f')
+        number = ch.charCodeAt(0) - 'a'.charCodeAt(0) + 10;
+    return number;
+}
 
-		try {
-			// Get Algorithm name
-			String desAlgoName = getDESAlgorithmName(ALGO_NAME);
-			// Create Cipher object
-			Cipher cipher = Cipher.getInstance(ALGO_NAME);
-			//Actual DES algo needs 56 bits key, which is equivalent to 1byte (0 at 0th position)  Get 8*3 byets key
-			byte [] key = hexFromString(SHARED_KEY);
-			System.out.println("DES Algorithm  shared key size in bytes >> "+key.length);
-			// Create SecretKeySpec
-			SecretKeySpec secretKeySpec = new SecretKeySpec(key, desAlgoName);
-			//Encrypt bytes
-			byte [] encryptedBytes = encryptIntoBytes(cipher, secretKeySpec, PLAIN_TEXT.getBytes(), 0, PLAIN_TEXT.getBytes().length);
-			String encryptedString=  hexToString(encryptedBytes);
-			System.out.println(encryptedString);
+function hexFromString(hex) {
+    var len = hex.length;
+    var buf = [];
+    var i = 0;
+    while (i < len) {
+        var charAtFirstPlace = hex.charAt(i++);
+        var charAtSecondPlace = hex.charAt(i++);
+        var byteData = (fromDigit(charAtFirstPlace) << 4) | fromDigit(charAtSecondPlace);
+        buf.push(byteData);
+    }
+    return buf;
+}
 
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+function hexToBufferArray(hex) {
+    var buf = hexFromString(hex);
+    var buffer = new Buffer(buf.length);
+    for (var i = 0; i < buf.length; i++) {
+        buffer[i] = buf[i];
+    }
+    return buffer;
+}
 
-	public static byte[] encryptIntoBytes(Cipher cipher, SecretKeySpec secretKeySpec, byte[] dct, int offset, int len) throws GeneralSecurityException {
-		cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-		byte[] ect = cipher.doFinal(addPadding(dct, offset, len));
-		return ect;
-	}
+var hexDigits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+function hexToString(ba, offset, length) {
+    var stringValue = "";
+    var j = 0;
+    var k;
+    for (var i = offset; i < offset + length; i++) {
+        k = ba[i];
+        var hexAtFirstPosition = hexDigits[(k >>> 4) & 0x0F];
+        var hexAtSecondPosition = hexDigits[k & 0x0F];
+        stringValue = stringValue + hexAtFirstPosition;
+        stringValue = stringValue + hexAtSecondPosition;
+    }
+    return stringValue;
+}
 
-	public static String getDESAlgorithmName(String algoName) {
-		System.out.println("getDESAlgorithmName algoName >> "+algoName);
-		String desAlgoName = null;
-		int i = algoName.indexOf("/");
-		if (i != -1)
-			desAlgoName = algoName.substring(0, i);
-		else
-			desAlgoName = algoName;
-		return desAlgoName;
-	}
+function addPadding(inData) {
+    var len = inData.length;
+    var bp = [];
+    var padChars = 8; // start with max padding value
+    var partial = (len + 1) % padChars; // calculate how many extra bytes exist
+    if (partial === 0) {
+        padChars = 1; // if none, set to only pad with length byte
+    } else {
+        padChars = padChars - partial + 1; // calculate padding size to include length
+    }
+//create bp array
+    var bp = new Buffer(len + padChars);
+    console.log("addPadding >> Add padding of " + padChars);
+    var padByte = fromDigit(padChars + "");
+    bp[0] = padByte;
+    //Add original array to dest array
+    for (var i = 0; i < len; i++) {
+        bp[i + 1] = inData[i];
+    }
+    return bp;
+}
 
-	/**
-	 * Adds padding characters to the data to be encrypted. Also adds random
-	 * Initial Value to the beginning of the encrypted data when using Triple
-	 * DES in CBC mode (DES-EDE3/CBC).
-	 * 
-	 * @param inData
-	 *            Array of bytes to be padded
-	 * @param offset
-	 *            Offset to starting point within array
-	 * @param len
-	 *            Number of bytes to be encrypted
-	 * @return Padded array of bytes
-	 */
-	public static byte[] addPadding(byte[] inData, int offset, int len) {
-		System.out.println("addPadding offset >> "+offset+", len >> "+len);
-		byte[] bp = null;
-		int padChars = PADDING_BLOCK; // start with max padding value
-		int partial = (len + 1) % padChars; // calculate how many extra bytes
-											// exist
-		if (partial == 0) {
-			padChars = 1; // if none, set to only pad with length byte
-		} else {
-			padChars = padChars - partial + 1; // calculate padding size to
-												// include length
-		}
-		System.out.println("addPadding >> Add padding of "+padChars);
-		/*
-		 * Create a byte array large enough to hold data plus padding bytes The
-		 * count of padding bytes is placed in the first byte of the data to be
-		 * encrypted. That byte is included in the count.
-		 */
-		bp = new byte[len + padChars];
-		bp[0] = Byte.parseByte(Integer.toString(padChars));
-		System.arraycopy(inData, offset, bp, 1, len);
-		return bp;
-	}
-
-	public static byte[] hexFromString(String hex) {
-		int len = hex.length();
-		byte[] buf = new byte[((len + 1) / 2)];
-
-		int i = 0, j = 0;
-		if ((len % 2) == 1)
-			buf[j++] = (byte) fromDigit(hex.charAt(i++));
-
-		while (i < len) {
-			buf[j++] = (byte) ((fromDigit(hex.charAt(i++)) << 4) | fromDigit(hex
-					.charAt(i++)));
-		}
-		return buf;
-	}
-
-	public static int fromDigit(char ch) {
-		if (ch >= '0' && ch <= '9')
-			return ch - '0';
-		if (ch >= 'A' && ch <= 'F')
-			return ch - 'A' + 10;
-		if (ch >= 'a' && ch <= 'f')
-			return ch - 'a' + 10;
-
-		throw new IllegalArgumentException("invalid hex digit '" + ch + "'");
-	}
-
-	public static String hexToString(byte[] ba) {
-		return hexToString(ba, 0, ba.length);
-	}
-
-	public static final char[] hexDigits = { '0', '1', '2', '3', '4', '5',
-			'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
-
-	public static String hexToString(byte[] ba, int offset, int length) {
-		char[] buf = new char[length * 2];
-		int j = 0;
-		int k;
-
-		for (int i = offset; i < offset + length; i++) {
-			k = ba[i];
-			buf[j++] = hexDigits[(k >>> 4) & 0x0F];
-			buf[j++] = hexDigits[k & 0x0F];
-		}
-		return new String(buf);
-	}
-
+function removePadding(inData) {
+    var bp = [];
+    var dataLength = inData.length;
+    var padLength = inData[0];
+    var dataLength = inData.length - padLength;
+    bp = new Buffer(dataLength);
+    for (var i = 0; i < dataLength; i++) {
+        bp[i + 1] = inData[i];
+    }
+    return bp;
 }
